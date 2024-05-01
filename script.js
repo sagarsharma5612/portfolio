04389c0f7ad4f6aed83b77243a6af694810e5706
Sagar Sharma <143262850+sagarsharma5612@users.noreply.github.com>
2024-05-01 21:14:41 +0530

    init-commit

--- script.js
+++ script.js
@@ -11,21 +11,33 @@ const p_btns = document.querySelector('.p-btn')
 
 const p_img_elem = document.querySelectorAll('.img-overlay')
 
+
+
+
+
 p_btns.addEventListener('click',(e) =>{
   const pbtn = e.target;
-  console.log(pbtn);
+  // console.log(pbtn);
+  
 
 
 
     const btn_num = pbtn.dataset.btnNum;
-    console.log(btn_num);
+    // console.log(btn_num);
 
     const img_active = document.querySelectorAll(`.btn-${btn_num}`)
 
     p_img_elem.forEach((value) => value.classList.add('p_img_not_active'))
+    
 
     img_active.forEach((value) => value.classList.remove('p_img_not_active'))
 
+})
+
+
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () =>{
 })
 
 
